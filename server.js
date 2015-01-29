a5ab81b53b6ead306ea29070359f76664bd6df9e
ML Kumorek <mk0934@it000530.users.bris.ac.uk>
2015-01-29 13:03:13 +0000

    Updated mock id

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ module.exports.startServer = function (config, configCallback, startCallback) {
 
 	// Point / to entry point
 	app.get('/', function(req, res) {
-	    res.sendFile(entryPoint, { root: destDir });
+	    res.sendFile(entryPoint, { root: './'+destDir });
 	});
 
 	// If we have extra config, call it before
 	if(configCallback) {
-		
-		configCallback(app, function () {	
+
+		configCallback(app, function () {
 			startServer(server, serverPort, startCallback);
 		});
 
@@ -35,7 +35,7 @@ module.exports.startServer = function (config, configCallback, startCallback) {
 
 var userInfo = {};
 userInfo[1] = {name: "Denis Ogun", profilePic: "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xaf1/v/t1.0-1/c117.0.320.320/p320x320/10561716_10154371608580398_1960362993738660566_n.jpg?oh=9d78ab899178310e5c2d3550a301ad63&oe=55247EE3&__gda__=1432709649_2c918b463cadac79835032700d5c0bb4"};
-userInfo[2] = {name: "Benji Barash", profilePic: "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xpa1/v/t1.0-1/c66.66.828.828/s111x111/558327_10152053694060381_1768235030_n.jpg?oh=9210147efc3f92684b9335cf22c64e5d&oe=55636B3F&__gda__=1428290679_638b26126cbe5ceb8381055a5ef5f1cf"};
+userInfo['fe71bfb393874d5d9828f044710c1744'] = {name: "Benji Barash", profilePic: "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xpa1/v/t1.0-1/c66.66.828.828/s111x111/558327_10152053694060381_1768235030_n.jpg?oh=9210147efc3f92684b9335cf22c64e5d&oe=55636B3F&__gda__=1428290679_638b26126cbe5ceb8381055a5ef5f1cf"};
 userInfo[3] = {name: "Ethan Katzenberg", profilePic: "https://media.licdn.com/mpr/mpr/shrink_100_100/p/1/005/09f/0f1/2d8692d.jpg"};
 
 // Start the server
@@ -52,19 +52,19 @@ function startServer(server, port, callback) {
 	});
 
 	client.on('data', function (data) {
-		
+
 		var availability = JSON.parse(data);
 		var users = availability.clients;
 		var availableUsers = [];
 
 		// Match IDs to user data
 		users.forEach(function (user) {
-			
+
 			if(user.status == "disconnected") {
 				return;
 			}
 
-			var info = userInfo[user.id];
+			var info = userInfo[user.id] || userInfo[0];
 			user.profilePic = info.profilePic;
 			user.name = info.name;
 			availableUsers.push(user);
@@ -78,10 +78,14 @@ function startServer(server, port, callback) {
 		console.log("Disconnected from BLE service");
 	});
 
+	client.on('error', function(err) {
+		console.log("Error during connection to the BLE servie", err);
+	});
+
 
 	process.on( 'SIGINT', function() {
 		console.log( "\nShutting down - SIGINT (Ctrl-C)" );
-	 	
+
 	 	// Close BLE service socket
 	 	client.end()
 

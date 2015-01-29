ae55110340beae476ec5f9ff4c5ecd0e59c5fa4d
Maciej Kumorek <mkumorek@gmail.com>
2015-01-29 13:09:14 +0000

    New id

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ module.exports.startServer = function (config, configCallback, startCallback) {
 
 var userInfo = {};
 userInfo[1] = {name: "Denis Ogun", profilePic: "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xaf1/v/t1.0-1/c117.0.320.320/p320x320/10561716_10154371608580398_1960362993738660566_n.jpg?oh=9d78ab899178310e5c2d3550a301ad63&oe=55247EE3&__gda__=1432709649_2c918b463cadac79835032700d5c0bb4"};
-userInfo['fe71bfb393874d5d9828f044710c1744'] = {name: "Benji Barash", profilePic: "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xpa1/v/t1.0-1/c66.66.828.828/s111x111/558327_10152053694060381_1768235030_n.jpg?oh=9210147efc3f92684b9335cf22c64e5d&oe=55636B3F&__gda__=1428290679_638b26126cbe5ceb8381055a5ef5f1cf"};
+userInfo['40f24d2ecb59485c87d78f3a343a4433'] = {name: "Benji Barash", profilePic: "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xpa1/v/t1.0-1/c66.66.828.828/s111x111/558327_10152053694060381_1768235030_n.jpg?oh=9210147efc3f92684b9335cf22c64e5d&oe=55636B3F&__gda__=1428290679_638b26126cbe5ceb8381055a5ef5f1cf"};
 userInfo[3] = {name: "Ethan Katzenberg", profilePic: "https://media.licdn.com/mpr/mpr/shrink_100_100/p/1/005/09f/0f1/2d8692d.jpg"};
 
 // Start the server

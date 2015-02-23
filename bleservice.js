933e9db53782e199bfe60d57a26af2af5c480cbe
Maciej Kumorek <mkumorek@gmail.com>
2015-02-23 17:16:32 +0000

    Receive new format of messages

--- bleservice.js
+++ bleservice.js
@@ -1,8 +1,39 @@
 var net = require('net');
 var JsonSocket = require('json-socket');
 var userApi = require('./server/users');
+var keyMirror = require('keymirror');
+
+var MessageCodes = keyMirror({
+  activePeripherals: null,
+  loginStatus: null
+});
+
 
 var socketPath = '/tmp/ble.sock';
+
+function handleActivePeripheralsMessage(io, users) {
+
+  var availableUsers = [];
+
+  // Send empty list
+  if(users.length === 0) {
+    // Push new state to web socket
+    io.emit('users', []);
+  }
+
+  // Match IDs to user data
+  users.forEach(function (user) {
+    userApi.getUserProfile(user.id, function(userProfie) {
+      availableUsers.push(userProfie);
+
+      // Do we now have all users?
+      if(availableUsers.length === users.length) {
+        // Push new state to web socket
+        io.emit('users', availableUsers);
+      }
+    });
+  });
+}
 
 module.exports.connectToBleService = function(io) {
 
@@ -14,34 +45,24 @@ module.exports.connectToBleService = function(io) {
 
     // Process message
     socket.on('message', function (message) {
+      console.log(message);
+      // Grab the message code
+      var code = message.code;
 
-      var users = message.clients;
-      var availableUsers = [];
+      switch(code) {
 
-      // Send empty list
-      if(users.length === 0) {
-        // Push new state to web socket
-        io.emit('users', []);
+          case MessageCodes.activePeripherals:
+            handleActivePeripheralsMessage(io, message.data.clients);
+            break;
+
+          case MessageCodes.loginStatus:
+            console.log("Login status", message.data);
+            break;
+
+          default:
+            // Don't know this message code
+            break;
       }
-
-      // Match IDs to user data
-      users.forEach(function (user) {
-
-        if(user.status == "disconnected") {
-          return;
-        }
-
-        userApi.getUserProfile(user.id, function(userProfie) {
-          availableUsers.push(userProfie);
-
-          // Do we now have all users?
-          if(availableUsers.length === users.length) {
-
-            // Push new state to web socket
-            io.emit('users', availableUsers);
-          }
-        });
-      });
     });
 
   });

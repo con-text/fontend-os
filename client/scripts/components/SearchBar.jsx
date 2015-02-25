3a352b988c385733487ce9711e16e2174649ad89
Maciej Kumorek <mkumorek@gmail.com>
2015-02-25 17:35:03 +0000

    Placeholder for search results

--- client/scripts/components/SearchBar.jsx
+++ client/scripts/components/SearchBar.jsx
@@ -12,11 +12,17 @@ var SearchBar = React.createClass({
   },
 
   render: function() {
+    var divClass = this.state.searchTerm.trim() === '' ? '' : 'active';
+
     return (
       <form id="searchBar" onSubmit={this.handleSubmit}>
         <input type="search" value={this.state.searchTerm}
           onChange={this.handleTermChange} />
+        <div id="searchResults" className={divClass}>
+          <h3>Searching for {this.state.searchTerm}</h3>
+        </div>
       </form>
+
     );
   },
 

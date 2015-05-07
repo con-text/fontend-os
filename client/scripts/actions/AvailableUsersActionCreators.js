6fa77ae3c293053cdeda18786bd6bed5ac249026
Maciej Kumorek <mkumorek@gmail.com>
2015-05-07 17:35:15 +0100

    Fixed available users being undefined

--- client/scripts/actions/AvailableUsersActionCreators.js
+++ client/scripts/actions/AvailableUsersActionCreators.js
@@ -1,14 +1,19 @@
 var AppDispatcher           = require('../dispatchers/AppDispatcher');
 var AvailableUsersConstants = require('../constants/AvailableUsersConstants');
 var ActionTypes = AvailableUsersConstants.ActionTypes;
-
+var _ = require('lodash');
 
 module.exports = {
 
   updateUsers: function(users) {
+
+    var availableUsers = _.filter(users, function(user) {
+      return _.isUndefined(user) === false;
+    });
+
     AppDispatcher.handleServerAction({
       type: ActionTypes.USERS_UPDATED,
-      users: users
+      users: availableUsers
     });
   }
 };
